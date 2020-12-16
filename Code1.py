d3f18190dad89d433fc0f3b8a58abee6605f37c0
wzeng0 <wzeng00@uchicago.edu>
2020-12-16 18:29:59 -0500

    Hello word --> different 3 basic types of data etc. String Boolean Number

---
+++ Code1.py
@@ -0,0 +1,22 @@
+# Prints Hello World into Console
+print("Hello World")
+
+# Prints a triangle into console in order
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+# Using Variables
+# variables storing the name and age in string
+# strings can store plain text, numbers, or boolean
+character_name = "William"
+character_age = 18
+is_male = False
+print("There once was a man named " + character_name + ", ")
+print("He was " + character_age + " years old. ")
+
+character_name = "Katie"
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
+
