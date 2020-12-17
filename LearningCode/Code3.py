4a97b505a08de380de3b122d1d5e34959a6278a7
wzeng0 <wzeng00@uchicago.edu>
2020-12-16 23:24:24 -0500

    Mathematic Functions

---
+++ LearningCode/Code3.py
@@ -0,0 +1,47 @@
+from math import *
+# accesses more math functions
+
+print(5)
+print(3.4)
+print(-3)
+print(4 + 10.3)
+print(3 - 5.4)
+print(3 * (4 + 5))
+print(3 * 4 + 5)
+
+# % means mod. 10 mod three finds the remainder in a division expression
+print(10 % 3)
+
+my_num = -5
+print(my_num)
+
+# converts my_num to a string
+print((str(my_num)) + " my favorite number")
+# cannot print((my_num) + " my favorite number") bc not string
+
+# abs means absolute value of a number
+print(abs(my_num))
+
+# pow finds the value of a certain number to a given power
+print(pow(my_num, 2))
+
+# max finds the larger of the numbers
+print(max(4, 6))
+print(max(4, my_num))
+
+# min finds the smaller of the numbers
+print(min(4, my_num))
+print(min(5, 4, 6))
+
+# round roudns the number to the nearest whole number
+print(round(3.6))
+print(round(4.3))
+
+# finds floor of number
+print (floor(3.7))
+
+# find ceiling of number
+print (ceil(3.7))
+
+# finds sqrt of number
+print(sqrt(36))
