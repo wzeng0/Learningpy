2848d965567d1f943d4785d773cbe9a4ca063e0a
wzeng0 <wzeng00@uchicago.edu>
2020-12-18 00:55:20 -0500

    Tuples

---
+++ LearningCode/Code7.py
@@ -0,0 +1,16 @@
+# tuples
+# container that we can store differnet values
+# similar to a list and can store multiple pieces of information
+# tuples are immutable meaning that it cannot be changed or modified
+
+# () creates tuples
+# these values inside this list cannot be changed
+coordinates = [(4, 5), (6, 7), (80, 34)]
+
+# can access items of tuples like lis
+print(coordinates[0])
+print(coordinates[1])
+
+# unlike lists, we cannot do coordinate[0] = 3 and expect coordinates = (3, 5)
+
+print(coordinates)
