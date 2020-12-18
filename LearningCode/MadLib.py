68e5d92fffb1ea67f4c9b51f966d59e333815fb8
wzeng0 <wzeng00@uchicago.edu>
2020-12-18 00:13:16 -0500

    MadLibs code

---
+++ LearningCode/MadLib.py
@@ -0,0 +1,18 @@
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a Plural Noun: ")
+celebrity = input("Enter a celebrity: ")
+
+# This is what a normal print looks like
+# print("Roses are red")
+# print("Violets are blue")
+# print("I love you")
+
+# madlib looks like
+# print("Roses are {color}")
+# print("{plural noun} are blue")
+# print("I love {celebrity}")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
