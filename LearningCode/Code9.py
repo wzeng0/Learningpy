1ffb639a7880df9612c123b99c989cf05a9ecf7e
wzeng0 <wzeng00@uchicago.edu>
2020-12-18 23:26:42 -0500

    Return function

---
+++ LearningCode/Code9.py
@@ -0,0 +1,13 @@
+
+# return a the cube of the given number
+# return value will not be printed unless the function is called
+# cannot type any code after return
+# python breaks code after return function is seen
+def cube(num):
+    return num*num*num
+
+# stores the value of clube of 4
+result = cube(4)
+
+# print the cube of 3
+print(result)
