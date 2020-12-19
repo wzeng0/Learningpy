cb55941a29f27fdcd28eac146d4a92a3f927c635
wzeng0 <wzeng00@uchicago.edu>
2020-12-18 23:09:51 -0500

    Intro to Functions

---
+++ LearningCode/Code8.py
@@ -0,0 +1,11 @@
+
+# a function that stores the code for printing "hello (name)
+# inside the def is the function name and a parameter
+# parameter must be entered when exeucting function
+
+def formalities(name, age):
+    print("Hello " + name + ". You are " + str(age) + " years old.")
+
+# calling/executing function
+formalities("William", 18)
+formalities("Emily", 20)
