29d485c2f0104adb2723c491825ac200cfeff267
wzeng0 <wzeng00@uchicago.edu>
2020-12-21 23:25:52 -0500

    Produces error when input is not a number

--- LearningCode/Calculator.py
+++ LearningCode/Calculator.py
@@ -1,9 +1,5 @@
 # making a calculator that returns a value based on the user's inputs
 
-num1 = input("Enter a value: ")
-op = input("Enter an operation: ")
-num2 = input("Enter another value: ")
-
 def calculate(num1, op, num2):
     if op == "+":
         return num1 + num2
@@ -21,5 +17,20 @@ def calculate(num1, op, num2):
         return num1 < num2
     else:
         return op + " is not an operator"
+# can catch invalid inputs using try and except
+try:
+    num1 = float(input("Enter a value: "))
+    op = input("Enter an operation: ")
+    num2 = float(input("Enter another value: "))
+    print(calculate(num1, op, num2))
 
-print(calculate(num1, op, num2))
+# normal except would be too simple and bad practice in python
+
+# catches the zero division error
+except ZeroDivisionError as err:
+    print(err)
+
+# catches the invalid value error
+except ValueError as error:
+    # print("Invalid Input. Try again.")
+    print(error)
