6f35eb903a8cae711276c5d2d994f2d8d88892cb
wzeng0 <wzeng00@uchicago.edu>
2020-12-31 22:24:13 -0500

    Objects and classes

---
+++ LearningCode/Class_Object.py
@@ -0,0 +1,9 @@
+class Friend:
+
+    def __init__(self, name, year, school, major, is_partying, gpa):
+        self.name = name
+        self.year = year
+        self.school = school
+        self.major = major
+        self.is_partying = is_partying
+        self.gpa = gpa
---
+++ LearningCode/Class_Object2.py
@@ -0,0 +1,8 @@
+# imports class friend from the file
+from Class_Object import Friend
+
+# creates a friend
+Friend1 = Friend("Shirley Yang", "Freshman", "Princeton University", "Bio?", False, 3.9)
+Friend1 = Friend("William Zeng", "Freshman", "University of Chicago", "CS", False, 3.6)
+
+print(Friend1)
